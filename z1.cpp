dc8cab0be44315511974cd23255ca0be7388fb52
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-26 09:02:53 +0530

    hfjgdudt

--- z1.cpp
+++ z1.cpp
@@ -5,7 +5,15 @@ int main()
     int arr[] = {12, 32, 213, 342, 124, 54, 56};
     for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
     {
-        cout << arr[i] << endl;
+        for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
     }
     return 0;
 }
